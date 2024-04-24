83b63d9e304e5da64e39a10865f2b3f473bed4fa
Aleksandr Podcherednik <startcpa1@yandex.ru>
2024-04-24 17:25:50 +0300

    add docker-compose.yaml, Dockerfile, env.example, readme.md

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-1+=%9(y@)-rzbnn%2b7w6ly@!b*ydelxttehef=xp6ag3)l@o_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
